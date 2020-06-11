3eef3d0efb1e541fc88786f4204085e78752ac5f
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-11 19:39:18 +0000

    Correction4 file 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	if (LDig < 6 && LDig != 0)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, abs(LDig));
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, abs(LDig));
 	}
 	return (0);
 }
