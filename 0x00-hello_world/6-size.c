a4a55e7353c62e2b2268f437dce5aa83dc4181b9
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-11 00:21:34 +0000

    add file 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Start
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("Size of a char:" sizeof(char)" byte (s)");
+printf("Size of an int:" sizeof(int)" byte (s)");
+printf("Size of a long int:" sizeof(float)" byte (s)");
+printf("Size of a long long int:" sizeof(double)" byte (s)");
+printf("Size of a float:" sizeof(float)" byte (s)");
+return (0);
+}
