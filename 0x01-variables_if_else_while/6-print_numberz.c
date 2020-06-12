486090c2e54120e8b1a5504c001d52d3d090c9c8
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-12 00:38:56 +0000

    Correction1 file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Start
+* Return: Always 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
+
