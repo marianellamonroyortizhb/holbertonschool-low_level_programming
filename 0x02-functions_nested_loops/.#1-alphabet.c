fec59d1a4b21d78f663ce9dbd3339e859ee4235d
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-16 22:56:25 +0000

    Add directory and change1 file 1

---
+++ 0x02-functions_nested_loops/.#1-alphabet.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.17543:1592166734
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *main - Prints alphabet
+ *Return: 0 Success
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
+
