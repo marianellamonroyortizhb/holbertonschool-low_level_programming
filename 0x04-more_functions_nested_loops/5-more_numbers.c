021c39485ab5eec7401e8a51445eb4fb177aed5a
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-18 23:12:49 +0000

    Add file 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *more_numbers - print numbers 0-9
+ *Return: number.
+ *
+ */
+void more_numbers(void)
+{
+	int k, u, d;
+
+	int c;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			u = c % 10;
+			if (c > 10)
+			{
+				d = c / 10;
+				_putchar(d + '0');
+			}
+			_putchar(u + '0');
+		}
+		_putchar('\n');
+	}
+}
