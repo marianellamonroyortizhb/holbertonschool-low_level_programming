136d0dcaf457e68608ebdedec78a54d9b8c86d2c
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-18 18:57:07 +0000

    Readme

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *mul - multiplicate two numbers
+ *Return: 1 or 0 depending on is uppercase or not.
+ *@a: First character to multiplicate
+ *@b: Second character to multiplicate
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
