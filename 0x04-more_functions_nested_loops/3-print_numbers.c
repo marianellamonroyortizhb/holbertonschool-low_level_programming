c1889b26bfa464c7f18b313df93a1cbb1b0df00d
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-18 19:46:39 +0000

    add file 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_numbers - print numbers 0-9
+ *Return: number.
+ *
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
