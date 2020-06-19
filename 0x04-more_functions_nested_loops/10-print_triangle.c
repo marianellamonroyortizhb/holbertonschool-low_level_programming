af17c262a5270aea7069c983698221828e5b3430
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-19 04:39:28 +0000

    end

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *print_triangle - print a triangle
+ *@size: size of the square.
+ *
+ */
+void print_triangle(int size)
+{
+	int i = 0, j = 0;
+
+	if (size < 0)
+		_putchar('\n');
+
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+
+			if (j >= (size - 1 - i))
+			{
+				_putchar('#');
+			}
+			else
+			{
+				_putchar (' ');
+			}
+			j++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
