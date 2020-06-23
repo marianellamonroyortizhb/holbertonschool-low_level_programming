f2abf9dfc3bb4380a01abebcbd1fa66270e0b705
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-23 20:49:21 +0000

    Add file 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *print_rev - Print a string in reverse
+ *
+ *@s: Strign to evaluate
+ *Return: 0 Success;
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	while (i != 0)
+	{
+		_putchar (s[i]);
+		i--;
+	}
+	_putchar ('\n');
+}
