3e617a4954b65c66cc5e801d4960cabc5b4265d0
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-23 21:34:18 +0000

    Change8 file 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,23 +2,23 @@
 /**
  *print_rev - Print a string in reverse
  *
- *@s: Strign to evaluate
+ *@str: Strign to evaluate
  *Return: 0 Success;
  */
 
-void print_rev(char *s)
+void print_rev(char *str)
 {
 	int i;
 
 	i = 0;
 
-	while (s[i] != '\0')
+	while (str[i] != '\0')
 	{
 		i++;
 	}
 	while (i >= 0)
 	{
-		_putchar(s[i]);
+		_putchar(str[i]);
 		i--;
 	}
 	_putchar('\n');
