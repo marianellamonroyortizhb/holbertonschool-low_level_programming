e7c9901e35bb32bd9b1859c23dd77e8b22c467d3
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-24 15:27:37 +0000

    Cahnge space in output

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int array[5];
 
-	array[0] = 98;
-	array[1] = 402;
-	array[2] = -198;
-	array[3] = 298;
-	array[4] = -1024;
+	array[0] = -198;
+	array[1] = 298;
+	array[2] = 402;
+	array[3] = -1024;
+	array[4] = 98;
 	print_array(array, 5);
 	return (0);
 }
--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,7 +14,7 @@ void print_array(int *a, int n)
 		printf("%d", a[count]);
 
 		if (count < n - 1)
-			printf(" , ");
+			printf(", ");
 	}
 	printf("\n");
 }
