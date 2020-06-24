f370f6eea841285fb02a54ee936cf3f71ca2bb6c
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-24 05:17:09 +0000

    add 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - check the code for Holberton School students.
+ *@a: character
+ *@n: integer
+ */
+void print_array(int *a, int n)
+{
+	int count = 0;
+
+	for (count = 0; count = n; count++)
+	{
+		printf("%d", a[count]);
+
+		if (count < n - 1)
+			printf(" , ");
+	}
+	printf("\n");
+}
