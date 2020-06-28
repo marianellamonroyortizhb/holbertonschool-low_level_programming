7845cb8e0bdeddd0e1544fa4184e5afbb1868a03
marianellamonroyortizhb <1880@holbertonschool.com>
2020-06-28 23:39:37 +0000

    change file 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,12 +12,9 @@ int _strcmp(char *s1, char *s2)
 
 	int count = 0;
 
-	if (s1[count] && s1[count])
+	while (s1[count] && s2[count] && s1[count] == s2[count])
 	{
-		if (s1[count] == s2[count])
-		{
-			count++;
-		}
+		count++;
 	}
 	return (s1[count] - s2[count]);
 }
