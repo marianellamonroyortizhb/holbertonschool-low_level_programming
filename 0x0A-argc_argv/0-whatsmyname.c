2ff1389ae660adb604089555ad47c89a82d1fa1c
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-07 02:55:38 +0000

    add file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - fuction that prints the name
+ *@argc: count the arguments
+ *@argv: array of pointers
+ *Return: 0 Success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
