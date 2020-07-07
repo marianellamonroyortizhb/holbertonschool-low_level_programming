63881edca05c521aea04e4de5a9ad207971d9e69
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-07 03:25:32 +0000

    add file 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - fuction that prints all the arguments
+ *@argc: count the arguments
+ *@argv: array of pointers
+ *Return: 0 Success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
