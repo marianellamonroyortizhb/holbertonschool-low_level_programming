465b94a26081b6cd6a19b0e5fe06f78a3ac1852b
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-08 13:05:05 +0000

    change 3 file 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	ptr = (int **) malloc(width * sizeof(int *));
 	if (!ptr)
 	{
-		free(ptr);
+/*		free(ptr); */
 		return (NULL);
 	}
 	if (width <= 0)
@@ -33,7 +33,7 @@ int **alloc_grid(int width, int height)
 
 			if (!ptr)
 			{
-				free(ptr);
+/*				free(ptr); */
 				return (NULL);
 			}
 
