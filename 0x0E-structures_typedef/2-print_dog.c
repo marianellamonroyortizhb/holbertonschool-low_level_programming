cfcdd83f5b3e028dd30e81797f9bd488664d64f5
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-14 05:26:09 +0000

    change file 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - print dog structure
+ * @d: dog structure
+ */
+void print_dog(struct dog *d)
+{
+	if (!d)
+	{
+		return;
+	}
+	else
+	{
+		d->name = NULL ? printf("Name: (nil\n") : printf("Name: %s\n", d->name);
+
+		printf("Age: %f\n", d->age);
+
+		d->owner = NULL ? printf("Owner: (nil\n") : printf("Owner: %s\n", d->owner);
+	}
+}
