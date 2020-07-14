0e3b89802430750263cfd739e2bff2c77e684a91
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-14 04:44:10 +0000

    change4 file 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,24 @@
 #ifndef djangodog
 #define djangodog
+
 /**
- *struct dog - structure
- *@name: dog name
- *@age: dog age
- *@owner: dog owner
+ * struct dog - structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-typedef struct dog dog_t;
 
 #endif
