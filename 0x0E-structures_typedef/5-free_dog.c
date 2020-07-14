e9502290dcad8b41cfd21ff1ae42767d4a976308
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-14 05:00:30 +0000

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * freedog - free memory dog structure
+ * @d: dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (!d)
+	{
+		return;
+	}
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
