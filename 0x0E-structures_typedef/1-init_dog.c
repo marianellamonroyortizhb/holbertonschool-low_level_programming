967cdeb6d8554ef16fbabf860ebabeadfdbeb946
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-14 04:50:16 +0000

    change4 fi2e 0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+
+/**
+ * init_dog - dog inizialitation
+ * d: dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (!d)
+	{
+		return (0);
+	}
+	
+	d->name = name;
+	d->age = age;
+	d->owner =owner;  
+}
