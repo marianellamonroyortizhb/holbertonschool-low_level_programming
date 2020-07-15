ad994daf7c8a75959ec83c304e832c387b44f575
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-15 23:37:24 +0000

    Change2 file 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,10 @@
 
 /**
  * print_name - function that prints a name.
- * @name: string with the name
+ * @name: string with the name.
  * @f: pointer
  */
-
 void print_name(char *name, void (*f)(char *))
-
 {
 	if (name && f)
 	{
