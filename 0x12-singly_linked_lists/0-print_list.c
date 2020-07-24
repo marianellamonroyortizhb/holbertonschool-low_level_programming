162cd550df4811ad360aa7f78fcf8689a9a8fff3
marianellamonroyortizhb <1880@holbertonschool.com>
2020-07-24 00:05:59 -0500

    add file 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,27 @@
 #include "lists.h"
 
 /**
- * print_list -
- *
- * Return: 
+ * print_list - prints all the elements of a list_t
+ *@h: Constant list
+ * Return: count of elements
  */
 size_t print_list(const list_t *h)
 {
+	size_t count;
 
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+			printf("\n");
+		}
+		else
+		{
+			printf("[%i] %s", h->len, h->str);
+			printf("\n");
+		}
+		h = h->next;
+	}
+	return (count);
 }
