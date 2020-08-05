2c38bb0117d9ad5c0198250bc72fd923808e839d
marianellamonroyortizhb <1880@holbertonschool.com>
2020-08-04 23:44:00 -0500

    Change definition when text_content is NULL in file 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,11 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
+	}
 
 	fd0 = open(filename, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0600);
 
