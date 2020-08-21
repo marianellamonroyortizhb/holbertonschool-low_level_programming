b880e45decde9ce224304ed94630663cef9af52f
marianellamonroyortizhb <1880@holbertonschool.com>
2020-08-21 11:45:44 -0500

    Fix betty

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code.
+ *@arc: number of arguments
+ *@arg: arguments
+ * Return: Always 0.
+*/
+int main(int arc, char *arg[])
+{
+	(void) arc;
+	(void) *arg;
+	return (0);
+}
