04bc73fcc68a2aa520a7856de22d8df92e27ccf1
marianellamonroyrtizhb <1880@holbertonschool.com>
2021-08-31 23:50:49 -0500

    Update file 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+
 /**
  * linear_search - Find a value in an array
  * @array: array
@@ -6,9 +7,10 @@
  * @value: Value to find
  * Return: -1 or index 
  */
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t a_ind;
+	unsigned int a_ind = 0;
 
 	if (!array)
 	{
@@ -16,7 +18,7 @@ int linear_search(int *array, size_t size, int value)
 	}
 	for (a_ind = 0; a_ind < size; a_ind++)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)a_ind, array[a_ind]);
+		printf("Value checked array[%d] = [%d]\n", a_ind, array[a_ind]);
 		if (array[a_ind] == value)
 		{
 			return (a_ind);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,12 +1,10 @@
 
-#ifndef __HOLB__
-#define __HOLB__
+#ifndef SEARCH_H
+#define SEARCH_H
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-void print_array(int *array, int three, int one);
 
-#endif /* __HOLB__ */
+#endif /*SEARCH_H*/
