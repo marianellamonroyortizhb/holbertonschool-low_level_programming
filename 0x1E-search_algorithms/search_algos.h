2468dc0d6d3738ea855ef793c459d31ecf12725b
marianellamonroyrtizhb <1880@holbertonschool.com>
2021-08-31 23:56:49 -0500

    Update search_algos-h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,7 @@
 #include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, int three  int one);
 
 #endif /*SEARCH_H*/
