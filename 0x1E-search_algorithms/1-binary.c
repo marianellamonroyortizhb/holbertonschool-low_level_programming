dd539647dd198f7fc3b0895b07b341582a6746d3
marianellamonroyortizhb <1880@holbertonschool.com>
2021-09-01 22:53:21 +0000

    Fix betty style in file 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 /**
  * binary_search - Function that searches for a value in a sorted
- * array of integers using the Binary search algorithm
+ * @print_array - Function that prints an array
  * @array: An array
  * @size: Array size
  * @value: Value to find
@@ -12,47 +12,42 @@ int binary_search(int *array, size_t size, int value)
 	size_t two = 0, one = 0, three = 0;
 
 	if (!array)
-	{
 		return (-1);
-	}
-
 	three = size - 1;
 
-
-	while (one <= three) 
+	while (one <= three)
 	{
 		two = (one + three)  / 2;
 		print_array(array, three, one);
+
 		if (array[two] < value)
-		{
 			one = two + 1;
-		}
 		else if (array[two] > value)
-		{
 			three = two - 1;
-		}
 		else
-		{
 			return (two);
-		}
 	}
 	return (-1);
 }
+
+/**
+ * print_array - Function that prints an array
+ * @array: An array
+ * @three: Integer
+ * @one: Integer
+ * Return: Prints an array
+ */
 
 void print_array(int *array, int three, int one)
 {
 	printf("Searching in array:");
 
-	while (one <= three) 
+	while (one <= three)
 	{
-		if (one == three) 
-		{
+		if (one == three)
 			printf(" %d", array[one]);
-		}
 		else
-		{
 			printf(" %d,", array[one]);
-		}
 		one++;
 	}
 	printf("\n");
