e2111ffbf566952a0378593ff99d3ebfcfa5e716
marianellamonroyrtizhb <1880@holbertonschool.com>
2021-08-31 23:25:30 -0500

    Update search_algos-h

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,26 +1,25 @@
 #include "search_algos.h"
-
 /**
  * linear_search - Find a value in an array
  * @array: array
  * @size: array size
- * @value: value to find
- * Return: -1 or size
+ * @value: Value to find
+ * Return: -1 or index 
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t a_size;
+	size_t a_ind;
 
 	if (!array)
 	{
 		return (-1);
 	}
-	for (a_size = 0; a_size < size; a_size++)
+	for (a_ind = 0; a_ind < size; a_ind++)
 	{
-		printf("Value checked array[%d] = [%d]\n", (int)a_size, array[a_size]);
-		if (array[a_size] == value)
+		printf("Value checked array[%d] = [%d]\n", (int)a_ind, array[a_ind]);
+		if (array[a_ind] == value)
 		{
-			return (a_size);
+			return (a_ind);
 		}
 	}
 	return (-1);
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,13 +1,12 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 
-#include <math.h>
-#include <stdlib.h>
+#ifndef __HOLB__
+#define __HOLB__
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-void printf_array(int *array, int b_index, int l_index);
-int jump_search(int *array, size_t size, int value);
+void print_array(int *array, int right, int left);
 
-#endif
+#endif /* __HOLB__ */
